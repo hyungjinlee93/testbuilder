d7da674c2e885b946b46ab662e481b8f22713611
Hyungjin Lee <hyungjinlee@Hyungjins-MBP.hlnet>
2019-10-02 17:53:26 -0700

    Simplify code and account for Visa and MasterCard

--- detectNetwork.js
+++ detectNetwork.js
@@ -12,16 +12,25 @@ var detectNetwork = function(cardNumber) {
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
   if(cardNumber.length === 14){
-    if(Number(cardNumber[0] + cardNumber[1]) === 38 || Number(cardNumber[0] + cardNumber[1]) === 39){
+    if((cardNumber[0] + cardNumber[1]) === '38' || (cardNumber[0] + cardNumber[1]) === '39'){
       return 'Diner\'s Club';
     }
   }
-
-  else if(cardNumber.length === 15){
-    if(Number(cardNumber[0] + cardNumber[1]) === 34 || Number(cardNumber[0] + cardNumber[1]) === 37){
+  if(cardNumber.length === 15){
+    if((cardNumber[0] + cardNumber[1]) === '34' || (cardNumber[0] + cardNumber[1]) === '37'){
       return 'American Express';
     }
   }
-
+  if(cardNumber.length === 13 || cardNumber.length === 16 || cardNumber.length === 19){
+    if(cardNumber[0] === '4'){
+      return 'Visa';
+    }
+  }
+  if(cardNumber.length === 16){
+    let firstTwo = cardNumber[0] + cardNumber[1];
+    if(firstTwo === '51' || firstTwo === '52' || firstTwo === '53' || firstTwo === '54' || firstTwo == '55'){
+      return 'MasterCard';
+    }
+  }
   // Once you've read this, go ahead and try to implement this function, then return to the console.
 };
